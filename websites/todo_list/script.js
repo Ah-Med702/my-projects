70e0bbc56e7547b3db6c3305f6c581bfc3f63260
Ah-Med702 <113292428+Ah-Med702@users.noreply.github.com>
2023-06-29 23:35:47 +0300

    finish media queries

--- websites/todo_list/script.js
+++ websites/todo_list/script.js
@@ -2,41 +2,40 @@ let input = document.getElementById("input");
 let todos = document.getElementById("todos");
 
 if (localStorage.item == null) {
-  list = [];
+    list = [];
 } else {
-  list = JSON.parse(localStorage.item);
+    list = JSON.parse(localStorage.item);
 }
 
 function addTodo() {
-  if (input.value != "") {
-    let item = {
-      title: input.value,
-    };
-    list.push(item);
-    localStorage.setItem("item", JSON.stringify(list));
-    input.value = "";
-    showValue();
-  }
+    if (input.value != "") {
+        let item = {
+            title: input.value,
+        };
+        list.push(item);
+        localStorage.setItem("item", JSON.stringify(list));
+        input.value = "";
+        showValue();
+    }
 }
 
 function deleteItem(i) {
-  list.splice(i, 1);
-  localStorage.item = JSON.stringify(list);
+    list.splice(i, 1);
+    localStorage.item = JSON.stringify(list);
 
-
-  showValue();
+    showValue();
 }
 
 function showValue() {
-  let todo = "";
-  for (i = 0; i < list.length; i++) {
-    todo += `
+    let todo = "";
+    for (i = 0; i < list.length; i++) {
+        todo += `
     <div class="item">
         <div>${i + 1} - ${list[i].title}</div>
         <button onclick="deleteItem(${i})" class="del" id="del">delete</button>
     </div>
     `;
-  }
-  todos.innerHTML = todo;
+    }
+    todos.innerHTML = todo;
 }
 showValue();
